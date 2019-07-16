8b25731a0d3836501c7b806157fc69934cbe1bd7
Rafael Castellanos-Welsh <mrrafael1912@gmail.com>
2019-07-16 15:55:00 -0700

    pythonanywhere

--- AmbitioUs/wsgi.py
+++ AmbitioUs/wsgi.py
@@ -11,6 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+os.environ['DJANGO_SETTINGS_MODULE'] = 'AmbitioUs.settings'
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "AmbitioUs.settings")
 
 application = get_wsgi_application()
